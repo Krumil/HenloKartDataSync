bed9998040bb7b658086c5172b7b3d0289843147
s.saletti <s.saletti@qzrstudio.com>
2024-05-29 16:51:58 +0200

    push

--- events/continuous_listening.py
+++ events/continuous_listening.py
@@ -6,26 +6,26 @@ import time
 def continuous_listening(latest_block):
     while True:
         try:
-            # Create new event filters from the latest block
-            race_finished_filter = contract.events.RaceFinished.create_filter(
-                fromBlock=latest_block
-            )
+
+            # Poll for new PlayerCommitted events
             player_committed_filter = contract.events.PlayerCommitted.create_filter(
                 fromBlock=latest_block
             )
-
-            # Poll for new events
-            race_finished_entries = race_finished_filter.get_new_entries()
             player_committed_entries = player_committed_filter.get_new_entries()
-
             for event in player_committed_entries:
                 handle_event(event)
+
+            # Poll for new RaceFinished
+            race_finished_filter = contract.events.RaceFinished.create_filter(
+                fromBlock=latest_block
+            )
+            race_finished_entries = race_finished_filter.get_new_entries()
 
             for event in race_finished_entries:
                 handle_event(event)
 
-            latest_block = web3.eth.block_number  # Update to the latest block
-            time.sleep(5)
+            latest_block = web3.eth.block_number
+            time.sleep(10)
         except Exception as e:
             print(f"An error occurred: {str(e)}")
-            time.sleep(5)
+            time.sleep(10)
