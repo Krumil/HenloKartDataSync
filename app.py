13ae820891ed68739545f275485ac78bf6b2c4c9
s.saletti <s.saletti@qzrstudio.com>
2024-05-31 10:04:42 +0200

    push

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-# app.py
-from config.db import database
-from events.continuous_listening import continuous_listening
+# from events.continuous_listening import continuous_listening
 from events.initial_fetch import initial_fetch
 
 
 def main():
     start_block = 14820784
     latest_block = initial_fetch(start_block)
-    continuous_listening(latest_block)
+    print("Latest block: ", latest_block)
+    # continuous_listening(latest_block)
 
 
 if __name__ == "__main__":
